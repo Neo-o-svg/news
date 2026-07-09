b0c98bd01b615d9112eae07f545a9a62a0875173
Кучерина Стефания <kaktusovickaktus5@gmail.com>
2026-07-09 20:26:31 +0300

    Pagiantion

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -6,25 +6,46 @@ import NewsBanner from '../../components/NewsBanner/NewsBanner'
 import { getNews } from '../../api/apiNews'
 import NewsList from '../../components/NewsList/NewsList'
 import Skeleton from '../../components/Skeleton/Skeleton'
+import Pagination from '../../components/Pagination/Pagination'
 
 const Main = () => {
 	const [news, setNews] = useState([])
 	const [isLoading, setIsLoading] = useState(true)
+	const [currentPage, setCurrentPage] = useState(1)
+	const totalPages = 10
+	const pageSize = 10
+
+	const fetchNews = async currentPage => {
+		try {
+			setIsLoading(true)
+			const response = await getNews(currentPage, pageSize)
+			setNews(response.news)
+			setIsLoading(false)
+		} catch (error) {
+			console.log(error)
+		}
+	}
 
 	useEffect(() => {
-		const fetchNews = async () => {
-			try {
-				setIsLoading(true)
-				const response = await getNews()
-				setNews(response.news)
-				setIsLoading(false)
-			} catch (error) {
-				console.log(error)
-			}
-		}
+		// eslint-disable-next-line react-hooks/set-state-in-effect
+		fetchNews(currentPage)
+	}, [currentPage])
 
-		fetchNews()
-	}, [])
+	const handleNextPage = () => {
+		if (currentPage < totalPages) {
+			setCurrentPage(currentPage + 1)
+		}
+	}
+
+	const handlePreviousPage = () => {
+		if (currentPage > 1) {
+			setCurrentPage(currentPage - 1)
+		}
+	}
+
+	const handlePageClick = pageNumber => {
+		setCurrentPage(pageNumber)
+	}
 
 	return (
 		<main className={styles.main}>
@@ -36,6 +57,15 @@ const Main = () => {
 					type="banner"
 				/>
 			)}
+
+			<Pagination
+				handlePreviousPage={handlePreviousPage}
+				handleNextPage={handleNextPage}
+				handlePageClick={handlePageClick}
+				totalPages={totalPages}
+				currentPage={currentPage}
+			/>
+
 			{!isLoading ? (
 				<NewsList news={news} />
 			) : (
@@ -44,6 +74,14 @@ const Main = () => {
 					type="item"
 				/>
 			)}
+
+			<Pagination
+				handlePreviousPage={handlePreviousPage}
+				handleNextPage={handleNextPage}
+				handlePageClick={handlePageClick}
+				totalPages={totalPages}
+				currentPage={currentPage}
+			/>
 		</main>
 	)
 }
