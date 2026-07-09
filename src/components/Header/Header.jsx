77e166803f7f6b9ca956dc4ad23d91396a2386b0
Кучерина Стефания <kaktusovickaktus5@gmail.com>
2026-07-09 18:25:00 +0300

    Ui

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { formateDate } from '../../helpers/formateDate'
+
+import styles from './styles.module.css'
+
+const Header = () => {
+	return (
+		<header className={styles.header}>
+			<h1 className={styles.title}>NEWS REACTIFY</h1>
+			{/* лучше реализовать с использованием useEffect, state и interval для тог чтобы эта дата менялась */}
+			<p className={styles.date}>{formateDate(new Date())}</p>
+		</header>
+	)
+}
+
+export default Header
---
+++ src/components/NewsBanner/NewsBanner.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { formatTimeAgo } from '../../helpers/formateTimeAgo'
+
+import Image from '../Image/Image'
+
+import styles from './styles.module.css'
+
+const NewsBanner = ({ item }) => {
+	return (
+		<div className={styles.banner}>
+			<Image image={item.image} />
+			<h3 className={styles.title}>{item.title}</h3>
+			<p className={styles.extra}>
+				{formatTimeAgo(item.published)} by {item.author}
+			</p>
+		</div>
+	)
+}
+
+export default NewsBanner
