182b8ea28b8eae1f45b4f25427c2ada4e1f1ac1a
Кучерина Стефания <kaktusovickaktus5@gmail.com>
2026-07-09 22:34:01 +0300

    Filtering

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import styles from './styles.module.css'
+
+const Categories = ({ categories, setSelectedCategory, selectedCategory }) => {
+	return (
+		<div className={styles.categories}>
+			{categories.map(category => {
+				return (
+					<button
+						key={category}
+						className={
+							selectedCategory === category ? styles.active : styles.item
+						}
+						onClick={() => setSelectedCategory(category)}
+					>
+						{category}
+					</button>
+				)
+			})}
+		</div>
+	)
+}
+
+export default Categories
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,24 +1,34 @@
 import React, { useEffect, useState } from 'react'
-import { formateDate } from '../../helpers/formateDate'
 
-import styles from './styles.module.css'
+import { getCategories, getNews } from '../../api/apiNews'
+
 import NewsBanner from '../../components/NewsBanner/NewsBanner'
-import { getNews } from '../../api/apiNews'
 import NewsList from '../../components/NewsList/NewsList'
+import Categories from '../../components/Categories/Categories'
+
 import Skeleton from '../../components/Skeleton/Skeleton'
 import Pagination from '../../components/Pagination/Pagination'
+
+import styles from './styles.module.css'
 
 const Main = () => {
 	const [news, setNews] = useState([])
 	const [isLoading, setIsLoading] = useState(true)
 	const [currentPage, setCurrentPage] = useState(1)
+	const [categories, setCategories] = useState([])
+	const [selectedCategory, setSelectedCategory] = useState('All')
+
 	const totalPages = 10
 	const pageSize = 10
 
 	const fetchNews = async currentPage => {
 		try {
 			setIsLoading(true)
-			const response = await getNews(currentPage, pageSize)
+			const response = await getNews({
+				page_number: currentPage,
+				page_size: pageSize,
+				category: selectedCategory === 'All' ? null : selectedCategory
+			})
 			setNews(response.news)
 			setIsLoading(false)
 		} catch (error) {
@@ -26,10 +36,24 @@ const Main = () => {
 		}
 	}
 
+	const fetchCategories = async () => {
+		try {
+			const response = await getCategories()
+			setCategories(['All', ...response.categories])
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	useEffect(() => {
+		// eslint-disable-next-line react-hooks/set-state-in-effect
+		fetchCategories()
+	}, [])
+
 	useEffect(() => {
 		// eslint-disable-next-line react-hooks/set-state-in-effect
 		fetchNews(currentPage)
-	}, [currentPage])
+	}, [currentPage, selectedCategory])
 
 	const handleNextPage = () => {
 		if (currentPage < totalPages) {
@@ -49,6 +73,12 @@ const Main = () => {
 
 	return (
 		<main className={styles.main}>
+			<Categories
+				categories={categories}
+				setSelectedCategory={setSelectedCategory}
+				selectedCategory={selectedCategory}
+			/>
+
 			{news.length > 0 && !isLoading ? (
 				<NewsBanner item={news[0]} />
 			) : (
