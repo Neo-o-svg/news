411aef4b7908294a4f67d7f1a2330ed5607dd43a
Кучерина Стефания <kaktusovickaktus5@gmail.com>
2026-07-14 21:10:38 +0300

    Refactor

--- src/components/LatestNews/LatestNews.jsx
+++ src/components/LatestNews/LatestNews.jsx
@@ -1,12 +1,15 @@
+import { getLatestNews } from '../../api/apiNews'
+import { useFetch } from '../../helpers/hooks/useFetch'
 import BannersList from '../BannersList/BannersList'
 
 import styles from './styles.module.css'
 
-const LatestNews = ({ isLoading, banners }) => {
+const LatestNews = () => {
+	const { data, isLoading } = useFetch(getLatestNews)
 	return (
 		<section className={styles.section}>
 			<BannersList
-				banners={banners}
+				banners={data?.news}
 				isLoading={isLoading}
 			/>
 		</section>
